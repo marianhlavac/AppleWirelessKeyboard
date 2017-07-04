e42c03edbac9055d5485b7479480a1bce68a3345
Marián Hlaváč <hlavac95@gmail.com>
2017-07-04 22:50:12 +0200

    Use keyboard events emulation instead of touching the Audio API

--- AppleWirelessKeyboard/VolumeControl.cs
+++ AppleWirelessKeyboard/VolumeControl.cs
@@ -1,48 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using CoreAudioApi;
+
 namespace AppleWirelessKeyboard
 {
     public static class VolumeControl
     {
-        static VolumeControl()
-        {
-            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
-            MMDevice device = enumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
-            Controller = device.AudioEndpointVolume;
-        }
-        private static AudioEndpointVolume Controller = null;
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
         public static void ToggleMute()
         {
-            Controller.Mute = !Controller.Mute;
-            if (Controller.Mute)
-                NotificationCenter.NotifyMuteOff();
-            else
-                NotificationCenter.NotifyMuteOn();
-
+            keybd_event((byte)Keys.VolumeMute, 0, 0, 0);
         }
         public static void VolumeUp()
         {
-            if (Controller.MasterVolumeLevelScalar > 0.9375f)
-                Controller.MasterVolumeLevelScalar = 1.0f;
-            else Controller.MasterVolumeLevelScalar += 0.0625f;
-            NotificationCenter.NotifyVolumeLevel((int)(Controller.MasterVolumeLevelScalar / 0.0625));
+            keybd_event((byte)Keys.VolumeUp, 0, 0, 0);
         }
         public static void VolumeDown()
         {
-            if (Controller.MasterVolumeLevelScalar < 0.0625)
-            {
-                Controller.MasterVolumeLevelScalar = 0;
-                NotificationCenter.NotifyNoVolume();
-            }
-            else
-            {
-                Controller.MasterVolumeLevelScalar -= 0.0625f;
-                NotificationCenter.NotifyVolumeLevel((int)(Controller.MasterVolumeLevelScalar / 0.0625));
-            }
+            keybd_event((byte)Keys.VolumeDown, 0, 0, 0);
         }
     }
 }
